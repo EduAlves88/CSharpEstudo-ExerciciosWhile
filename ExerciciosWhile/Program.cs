70d22a3f0189dbe722be869133c1615fc529297a
Eduardo Alves <edu.alves88@outlook.com>
2021-08-25 20:12:10 -0300

    Primeiro Exercício Realizado, acesso por senha!

--- ExerciciosWhile/Program.cs
+++ ExerciciosWhile/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ExerciciosWhile
 {
@@ -6,7 +7,31 @@ namespace ExerciciosWhile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Escreva um programa que repita a leitura de uma senha até que ela seja válida.
+            //Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida".
+            //Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido"
+            //e o algoritmo encerrado.Considere que a senha correta é o valor 2002.
+            int acerto;
+            bool controle;
+            int senha = 2002;
+            controle = true;
+            while (controle)
+            {                
+                Console.WriteLine("Entre com a senha ->");
+                acerto = int.Parse(Console.ReadLine());   
+                if (acerto == senha)
+                {
+                    Console.WriteLine("Acesso Permitido");
+                    controle = false;
+                }
+                else
+                {
+                    Console.WriteLine("Senha Inválida");
+                }
+            }
+
+
+
         }
     }
 }
