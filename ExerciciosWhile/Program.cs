0dac3c889d7576ae1fcf5175018f9e5464528ea8
Eduardo Alves <edu.alves88@outlook.com>
2021-08-31 20:32:01 -0300

    Exercicio 2 While list

--- ExerciciosWhile/Program.cs
+++ ExerciciosWhile/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Globalization;
 
+
+//Lista de Exercícios 4
+//Eduardo Reis Alves
+
+
 namespace ExerciciosWhile
 {
     class Program
@@ -16,9 +21,9 @@ namespace ExerciciosWhile
             int senha = 2002;
             controle = true;
             while (controle)
-            {                
-                Console.WriteLine("Entre com a senha ->");
-                acerto = int.Parse(Console.ReadLine());   
+            {
+                Console.WriteLine("Entre com a senha (Dica2002)->");
+                acerto = int.Parse(Console.ReadLine());
                 if (acerto == senha)
                 {
                     Console.WriteLine("Acesso Permitido");
@@ -29,9 +34,41 @@ namespace ExerciciosWhile
                     Console.WriteLine("Senha Inválida");
                 }
             }
+            //Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema cartesiano.
+            //Para cada ponto escrever o quadrante a que ele pertence.
+            //O algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA(nesta situação sem escrever mensagem alguma).
 
+            bool looper = true;
 
+            while (looper)
+            {
+                Console.WriteLine("Entre com o valor de X e Y:");
+                string coordXY = Console.ReadLine();
+                string[] vetCoord = coordXY.Split(' ');
+                float valX = float.Parse((vetCoord[0]), CultureInfo.InvariantCulture);
+                float valY = float.Parse((vetCoord[1]), CultureInfo.InvariantCulture);
+                if ((valX == 0) || (valY == 0))
+                {
+                    looper = false;
+                }
+                else if ((valX < 0) && (valY > 0))
+                {
+                    Console.WriteLine("Primeiro Quadrante!");// Q1
+                }
+                else if ((valX > 0) && (valY > 0))
+                {
+                    Console.WriteLine("Segundo Quadrante!");//Q2
+                }
+                else if ((valX < 0) && (valY < 0))
+                {
+                    Console.WriteLine("Terceiro Quadrante!");//Q3
+                }
+                else
+                {
+                    Console.WriteLine("Quarto Quadrante!");//Q4
+                }
 
+            }
         }
     }
 }
