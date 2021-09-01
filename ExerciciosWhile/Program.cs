b951af8535326c4bc47eb3bd41f45ea85f06aae1
Eduardo Alves <edu.alves88@outlook.com>
2021-08-31 21:32:29 -0300

    Exercicio while de abastecimento e contagem

--- ExerciciosWhile/Program.cs
+++ ExerciciosWhile/Program.cs
@@ -68,7 +68,52 @@ namespace ExerciciosWhile
                     Console.WriteLine("Quarto Quadrante!");//Q4
                 }
 
-            }
+            }//fim while
+
+            //Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva
+            //um algoritmo para ler o tipo de combustível abastecido(codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel
+            //4.Fim). Caso o usuário informe um código inválido(fora da faixa de 1 a 4) deve ser solicitado um novo código(até
+            //que seja válido). O programa será encerrado quando o código informado for o número 4.Deve ser escrito a
+            //mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível
+            bool abastecimento = true;
+            int contAlcool = 0;
+            int contGasolina = 0;
+            int contDiesel = 0;
+            while (abastecimento)
+            {
+                Console.WriteLine("Entre com o abastecimento: ");
+                Console.WriteLine("1 - Álcool");
+                Console.WriteLine("2 - Gasolina");
+                Console.WriteLine("3 - Diesel");
+                Console.WriteLine("4 - Fim");
+                int abasteceu = int.Parse(Console.ReadLine());
+                if (abasteceu == 1)
+                {
+                    contAlcool += 1;
+                }
+                if (abasteceu == 2)
+                {
+                    contGasolina += 1;
+                }
+                if (abasteceu == 3)
+                {
+                    contDiesel += 1;
+                }
+                if (abasteceu == 4)
+                {
+                    Console.WriteLine("Muito Obrigado!");
+                    Console.WriteLine("Álcool = " + contAlcool);
+                    Console.WriteLine("Gasolina = " + contGasolina);
+                    Console.WriteLine("Diesel = " + contDiesel);
+                    abastecimento = false;
+                }
+
+
+            }//fim while
+
+
+
+
         }
     }
 }
